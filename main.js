505a20bbcbbb520cd7363dbc2799698fa1dfaed0
ttakkku <k2005118@gmail.com>
2019-05-03 23:40:47 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log("Ddd");
+//https://www.youtube.com/watch?v=M4qfREjyeq0&list=PLlSZlNj22M7S1HmF3Vs8TJ2gUq_0xNN6-&index=4
---
+++ main.ts
@@ -0,0 +1,17 @@
+//type
+let myname = "ttakkku";
+// myname = 35;
+
+let myage = 35;
+// myage = '35';
+
+let ishecome = false;
+// ishecome = 'js';
+
+let hisbag: any;
+hisbag = 35;
+hisbag = "ttakkku";
+/**
+ * @next https://www.youtube.com/watch?v=lmWufxmmLl8&list=PLlSZlNj22M7S1HmF3Vs8TJ2gUq_0xNN6-&index=5
+ * TypeScript 005 Array
+ */
